19e72b808c6a4fa2df3a90c082fe796627cea8f2
tamdaominh0218 <mtdd1802@gmail.com>
2025-08-28 02:32:42 +0700

    add circular_doubly

---
+++ Source/data_structure/Part11_Liked_list/Circular_doubly_linked_list.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	Node* prev;
+	int data;
+	Node* next;
+};
+
+void insertEnd(Node** head, int val) {
+	Node* newNode = new Node;
+	newNode->data = val;
+	if (*head==nullptr)
+	{
+		*head = newNode;
+		newNode->next = *head;
+		return;
+	}
+
+	Node* temp = *head;
+	while (temp->next!=*head)
+	{
+		temp = temp->next;
+	}
+	temp->next = newNode;
+	newNode->prev = temp;
+	newNode->next = *head;
+	(*head)->prev = newNode;
+}
+
+void display(Node* head) {
+	Node* temp = head;
+	do
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	} while (temp!=head);
+	cout << endl;
+}
+
+int main() {
+	Node* n1 = nullptr;
+	insertEnd(&n1, 5);
+	insertEnd(&n1, 6);
+	insertEnd(&n1, 7);
+	insertEnd(&n1, 8);
+	insertEnd(&n1, 9);
+	insertEnd(&n1, 10);
+	display(n1);
+
+	return 0;
+}
